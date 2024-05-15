50a1f4092cf6a54a429ea457982614a04d54f8f8
Hilman Jihadi S.Kom., MMSI <91356569+hilmansystem@users.noreply.github.com>
2024-05-15 10:29:51 +0700

    Update koneksi.php
    UPDATE JIKA DIHOSTING

--- multi_user/koneksi.php
+++ multi_user/koneksi.php
@@ -7,3 +7,25 @@ if (mysqli_connect_errno()){
 }
  
 ?>
+
+
+
+// UNTUK HOSTING GUNAKAN FILE DIBAWAH, UNTUK LOKAL HOST XAMPP BIASA GUNAKAN FILE DIATAS, DAN HAPUS YANG TIDAK DIPAKAI ATAS ATAU BAWAH GUNAKAN SALAHSATU.
+
+<?php
+// Konfigurasi koneksi ke database
+$host = "sql112.infinityfree.com"; // Host MySQL
+$username = "if0_36550349"; // Username MySQL
+$password = "YSSdZuW3MNxNibS"; // Password MySQL
+$database = "if0_36550349_multi_user"; // Nama Database
+
+// Buat koneksi ke database
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+// Periksa koneksi
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+ 
+?>
